2a45bf212656c19b398f71ae5953b7a9f3ab2f29
Sidney Lopes <sidneylopesjr@gmail.com>
2022-09-05 17:57:09 -0300

    feat: mudando a estrutura do boleto pjbank

--- src/OpenBoleto/Banco/PJBank.php
+++ src/OpenBoleto/Banco/PJBank.php
@@ -29,134 +29,60 @@ namespace OpenBoleto\Banco;
 use OpenBoleto\BoletoAbstract;
 use OpenBoleto\Exception;
 
-/**
- * Classe boleto Unicred.
- *
- * @package    OpenBoleto
- * @author     Victor Feitoza <http://github.com/vfeitoza>
- * @copyright  Copyright (c) 2013 Estrada Virtual (http://www.estradavirtual.com.br)
- * @license    MIT License
- * @version    1.0
- */
 class PJBank extends BoletoAbstract
 {
-    /**
-     * Código do banco
-     * @var string
-     */
     protected $codigoBanco = '301';
 
-    /**
-     * Localização do logotipo do banco, referente ao diretório de imagens
-     * @var string
-     */
     protected $logoBanco = 'sicoob.jpg';
 
-    /**
-     * Define as carteiras disponíveis para este banco
-     * @var array
-     */
     protected $carteiras = array('1', '2', '5');
 
-    /**
-     * Convênio utilizado pelo Sacado
-     * @var integer
-     */
     protected $convenio = '12345';
 
-    /**
-     * Número de parcelas usadas no boleto ou carnê
-     * @var string
-     */
     protected $numParcelas = '001';
 
-    /**
-    * Linha de local de pagamento
-    * @var string
-    */
-    protected $localPagamento = 'Pagável preferencialmente no Sicoob';
+    protected $localPagamento = 'Pagável em qualquer banco.';
 
-    /**
-     * Opções de modalidade aceitas pelo banco
-     * @var array
-     */
     protected $modalidades = array('01','02','05');
-    
-    /**
-     * Gera o Nosso Número.
-     *
-     * @return string
-     */
+
+    protected $codigo_de_barras;
+
+    public function setCodigoDeBarras($codigo_de_barras)
+    {
+        $this->codigo_de_barras = $codigo_de_barras;
+        return $this;
+    }
+
+    public function getCodigoDeBarras()
+    {
+        return $this->codigo_de_barras;
+    }
+
+    public function getNumeroFebraban()
+    {
+        return $this->codigo_de_barras;
+    }
+
+    public function getNossoNumero($incluirFormatacao = true)
+    {
+        return substr_replace($this->nosso_numero, ' ' . substr($this->nosso_numero, -1), 12, 1);
+    }
+
     protected function gerarNossoNumero()
     {
-        $numero = self::zeroFill($this->getSequencial(), 7);
-        $sequencia = $this->getAgencia() . self::zeroFill($this->getConvenio(), 10) . $numero;
-
-        $cont=0;
-        $calculoDv = '';
-        // Constante para cálculo  = 3197
-        // c) Multiplicar cada componente da seqüência com o seu correspondente da constante e somar os resultados.
-        for ($num = 0; $num <= strlen($sequencia); $num++) {
-            $cont++;
-            if ($cont == 1) {
-                // constante fixa Sicoob » 3197
-                $constante = 3;
-            }
-            if ($cont == 2) {
-                $constante = 1;
-            }
-            if ($cont == 3) {
-                $constante = 9;
-            }
-            if ($cont == 4) {
-                $constante = 7;
-                $cont = 0;
-            }
-            $calculoDv = (int)$calculoDv + ((int)substr($sequencia, $num, 1) * $constante);
-        }
-        // c) Multiplicar cada componente da seqüência com o seu correspondente da constante e somar os resultados.
-        $resto = $calculoDv % 11;
-
-        // e) O resto da divisão deverá ser subtraído de 11 achando assim o DV (Se o Resto for igual a 0 ou 1 então o DV é igual a 0).        
-        if ( ($resto == 0) || ($resto == 1) ) {
-            $dv = 0;
-        } else {
-            $dv = 11 - $resto;
-        }
-
-        return $numero .'-'. $dv;
-        
+       return $this;
     }
 
-    /**
-     * Método para gerar o código da posição de 20 a 44
-     *
-     * @return string
-     * @throws \OpenBoleto\Exception
-     */
+    protected function getDigitoVerificador()
+    {
+        return $this;
+    }
+
     public function getCampoLivre()
     {
-        return $this->getCarteira(). $this->getAgencia() . self::zeroFill($this->getCarteira(),2) . self::zeroFill($this->getConvenio(), 7) .
-               $this->getNossoNumero(false) . self::zeroFill($this->getNumParcelas(), 3);
+        return $this;
     }
 
-    /**
-     * Retorna o campo Agência/Cedente do boleto
-     *
-     * @return string
-     */
-    public function getAgenciaCodigoCedente()
-    {
-        return static::zeroFill($this->getAgencia(), 4) . ' / ' . $this->getConvenio();
-    }
-
-    /**
-     * Define a modalidade da carteira
-     *
-     * @param type $modalidade
-     * @return \OpenBoleto\Banco\Sicoob
-     * @throws Exception
-     */
     public function setModalidade($modalidade)
     {
         if (!in_array($modalidade, $this->getModalidades())) {
@@ -168,60 +94,34 @@ class PJBank extends BoletoAbstract
         return $this;
     }
 
-    /**
-     * seta o convênio a ser utilizado pelo Sacado
-     *
-     * @param integer $convenio Convẽnio do sacado
-     * @return \OpenBoleto\Banco\Sicoob
-     */
     public function setConvenio($convenio) {
         $this->convenio = $convenio;
 
         return $this;
     }
 
-    /**
-     * Retorna a modalidade da carteira
-     *
-     * @return string
-     */
     public function getModalidade()
     {
         return $this->modalidade;
     }
 
-    /**
-     * Retorna todas as modalidades disponíveis
-     *
-     * @return array
-     */
     public function getModalidades()
     {
         return $this->modalidades;
     }
 
-    /**
-     * Retorna o número de parcelas
-     *
-     * @return string
-     */
     public function getNumParcelas()
     {
         return $this->numParcelas;
     }
 
-    /**
-     * Retorna o convênio do Sacado
-     *
-     * @return integer
-     */
-    public function getConvenio()
-    {
-        return $this->convenio;
-    }
-
     public function setCarteira($carteira)
     {
         return $this;
+    }
+
+    public function getConvenio()
+    {
+        return $this->convenio;
     }
 }
